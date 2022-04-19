b21e4575591a24aa5d591c223adcbd8e91a7c7d0
Felipe Sousa <csfelipe8@gmail.com>
2022-04-19 13:47:27 -0300

    setup complete

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current'
+          }
+        }
+      ],
+      '@babel/preset-typescript'
+    ],
+    plugins: [
+      ['module-resolver', {
+        alias: {
+          '@config': './src/config',
+          '@providers': './src/providers',
+          '@entities': './src/entities',
+          '@repositories': './src/repositories',
+          '@usecases': './src/usecases'
+        }
+      }]
+    ],
+    ignore: [
+      '**/*.spec.ts'
+    ]
+  }
