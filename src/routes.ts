e700e234c69a228755135fad62b02020f68c49e6
Felipe Sousa <54862789+FelipeJSousa@users.noreply.github.com>
2022-04-20 03:47:32 -0300

    fix(routes): fix path Agentes

--- src/routes.ts
+++ src/routes.ts
@@ -5,10 +5,10 @@ const router = Router();
 
 router.get('/', (request, response) => response.json({ message: 'services-agente-parceiro-magalu' }));
 
-router.post('/users', RegisterAgentesController.Save);
-router.put('/users', RegisterAgentesController.Save);
-router.delete('/users', RegisterAgentesController.Delete);
-router.get('/users', RegisterAgentesController.Get);
-router.get('/users/:id', RegisterAgentesController.Get);
+router.post('/Agente', RegisterAgentesController.Save);
+router.put('/Agente', RegisterAgentesController.Save);
+router.delete('/Agente', RegisterAgentesController.Delete);
+router.get('/Agente', RegisterAgentesController.Get);
+router.get('/Agente/:id', RegisterAgentesController.Get);
 
 export { router };
