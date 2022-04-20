0bda61fa02db3146e4db78b4221e2a3db9fee66e
Felipe Sousa <54862789+FelipeJSousa@users.noreply.github.com>
2022-04-20 03:33:59 -0300

    feat(server): configure server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
-import express, { application } from 'express';
+import { Env } from '@config/environment';
+import { ErrorHandling } from '@middlewares/errorHandling';
+import express, { json } from 'express';
 import { router } from './routes';
 
 const app = express();
 
-app.routes(router);
+app.use(json());
 
-app.listen(3333);
+app.use(router);
+
+app.use(ErrorHandling);
+
+app.listen(Env.PORT, () => {
+	if (!Env.PORT)
+		throw new Error('Port is undefined.');
+	console.log(`server runnig in ${Env.PORT}`);
+});
