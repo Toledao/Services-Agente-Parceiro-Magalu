38fe595df98011f92950d900fc0f7911591ec4b3
Felipe Sousa <csfelipe8@gmail.com>
2022-04-30 20:19:34 -0300

    fix(entity): Agente omit update

--- src/entities/agente.ts
+++ src/entities/agente.ts
@@ -13,7 +13,7 @@ export class Agente extends Entity {
 	parceiro: Parceiro[];
 	roteiro: Roteiro[];
 
-	constructor(props: Omit<Agente, 'id'>, id?: string) {
+	constructor(props: Omit<Agente, 'id' | 'parceiro' | 'roteiro'>, id?: string) {
 		super(id);
 		Object.assign(this, props);
 	}
