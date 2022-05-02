9f5f8019ae18f73cf8487b7885d7a266770d0bf6
Felipe Sousa <54862789+FelipeJSousa@users.noreply.github.com>
2022-05-02 18:09:47 -0300

    feat(Roteiro): Add GetUseCase

---
+++ src/useCases/RoteiroUseCases/GetAgenteUseCase.ts
@@ -0,0 +1,22 @@
+import { Roteiro } from '@entities/roteiro';
+import { IRoteirosRepository } from '@repositories/IRoteirosRepository';
+import { IRoteiroQueryRequestDTO, RoteiroResponseDTO } from './RoteiroDTO';
+
+export class GetRoteiroUseCase {
+
+	constructor(
+		private readonly roteirosRepository: IRoteirosRepository
+	) { }
+
+	async execute(data?: IRoteiroQueryRequestDTO): Promise<RoteiroResponseDTO[]> {
+
+		if (data?.id === undefined || data?.id === '') {
+			const ret = await this.roteirosRepository.getByFilter(<Roteiro>{ ...data });
+			return ret.length === 0 ? [] : ret.map(x => new RoteiroResponseDTO(x));
+		}
+
+		const ret = await this.roteirosRepository.getById(data.id);
+
+		return !ret?.id ? [] : Array<RoteiroResponseDTO>(new RoteiroResponseDTO(ret));
+	}
+}
